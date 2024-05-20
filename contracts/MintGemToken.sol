fc7aa819e2ed1269abd588f644efb3fa4d609a1c
hyundo0328 <hyundo13@naver.com>
2024-05-21 01:21:11 +0900

    ✨ Feature: NFT 대량 발행 (#3)

--- contracts/MintGemToken.sol
+++ contracts/MintGemToken.sol
@@ -6,23 +6,24 @@ pragma solidity ^0.8.25;
 // ERC721Enumerable : ERC721의 확장, utility 버전
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 // remixd -s . --remix-ide https://remix.ethereum.org
 // https://remix.ethereum.org/?#activate=klaytn-remix-plugin,fileManager
 contract MintGemToken is ERC721Enumerable, Ownable{
-  uint constant public MAX_TOKEN_COUNT = 3;
+  uint constant public MAX_TOKEN_COUNT = 10000;
   uint constant public TOKEN_RANK_LENGTH = 4;
   uint constant public TOKEN_TYPE_LENGTH = 4;
 
   string public metadataURI;
 
   // 10^18 peb = 1 Klay
-  uint public gemTokenPrice = 1000000000000000000;
+  uint constant public GEM_TOKEN_PRICE = 1000000000000000000;
+  // 1000000000000000000
 
-  //
-  constructor(string memory _name, string memory _symbol, string memory _metadataURI) ERC721(_name, _symbol) Ownable(msg.sender) {
-    metadataURI = _metadataURI;
+  // string memory _metadataURI
+  constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) Ownable(msg.sender) {
+
   }
 
   struct GemTokenData{
@@ -43,21 +44,24 @@ contract MintGemToken is ERC721Enumerable, Ownable{
     return string(abi.encodePacked(metadataURI, '/', gemTokenRank, '/', gemTokenType, '.json'));
   }
 
-  function mintGemToken() public payable{
-    require(gemTokenPrice <= msg.value, "Not Enough Klay.");
-    require(MAX_TOKEN_COUNT > totalSupply(), "No more minting is possible");
+  function mintGemToken(uint amount) public payable{
+    for (uint i=0;i<amount;i++){
+      require(GEM_TOKEN_PRICE <= msg.value, "The fee amount is insufficient.");
+      require(MAX_TOKEN_COUNT > amount, "You exceed the limited quantity.");
 
-    uint tokenId = totalSupply() + 1;
+      uint tokenId = totalSupply() + 1;
 
-    GemTokenData memory randomTokenData = randomGenerator(msg.sender, tokenId);
+      // GemTokenData memory randomTokenData = randomGenerator(msg.sender, tokenId);
 
-    gemTokenData[tokenId] = GemTokenData(randomTokenData.gemTokenRank, randomTokenData.gemTokenType);
+      // gemTokenData[tokenId] = GemTokenData(randomTokenData.gemTokenRank, randomTokenData.gemTokenType);
 
-    gemTokenCount[randomTokenData.gemTokenRank - 1][randomTokenData.gemTokenType - 1] += 1;
+      // gemTokenCount[randomTokenData.gemTokenRank - 1][randomTokenData.gemTokenType - 1] += 1;
 
-    payable(owner()).transfer(msg.value); // NFT 발행시 스마트 컨트랙트의 Owner에게 전송되는 수수료
+      // payable(owner()).transfer(msg.value); // NFT 발행시 스마트 컨트랙트의 Owner에게 전송되는 수수료값 설정
 
-    _mint(msg.sender, tokenId);
+      /* _mint 대신에 _safeMint로 변경 : 보안적으로 더욱 안전하게 Mint 가능 */
+      _safeMint(msg.sender, tokenId); // mint 실행 메소드
+    }
   }
 
   function getGemTokenCount() public view returns(uint[TOKEN_RANK_LENGTH][TOKEN_TYPE_LENGTH] memory){
