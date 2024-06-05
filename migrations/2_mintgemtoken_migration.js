42ee3f4566864b155bbe75febe0814ab57c02ee9
hyundo0328 <hyundo13@naver.com>
2024-06-06 01:20:38 +0900

    feat: Deploy and Mint of NFT of JS

--- migrations/2_mintgemtoken_migration.js
+++
@@ -1,8 +0,0 @@
-const mintGemToken = artifacts.require('../contracts/MintGemToken.sol');
-
-const concertName = "practice0601";
-const managerName = "park";
-
-module.exports = function(deployer) {
-	deployer.deploy(mintGemToken, concertName, managerName); 
-};
